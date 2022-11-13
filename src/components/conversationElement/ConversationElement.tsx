08cde956212278ee4341851d9dd6a34d8907128d
Alexandra <bug.graph@gmail.com>
2022-11-13 02:32:03 +0100

    fix: nickname in conversation

--- src/components/conversationElement/ConversationElement.tsx
+++ src/components/conversationElement/ConversationElement.tsx
@@ -2,22 +2,27 @@ import {Conversation} from "../../types/conversation";
 
 import styles from './ConversationElement.module.scss';
 import {Avatar} from "antd";
+import {useContext} from "react";
+import {UserContext} from "../../contexts/UserContext";
 
 export default function ConversationElement({conversation}: { conversation: Conversation }) {
+  const {userId} = useContext(UserContext);
 
   function renderLastMessageTimestamp() {
     return new Date(conversation.lastMessageTimestamp * 1000).toLocaleDateString()
   }
 
+  const currentNickname = userId === conversation.senderId ? conversation.recipientNickname : conversation.senderNickname
+
 
   return <a className={styles.conversationElement}>
     <Avatar
-      src={`https://joeschmoe.io/api/v1/${conversation.recipientNickname}`}
+      src={`https://joeschmoe.io/api/v1/${currentNickname}`}
       shape="circle"
       style={{width: "4.5em", height: "4.5em", backgroundColor: 'white'}}
     />
     <div className={styles.info}>
-      <span className={styles.nickname}>{conversation.recipientNickname}</span>
+      <span className={styles.nickname}>{currentNickname}</span>
       <span>{renderLastMessageTimestamp()}</span>
     </div>
   </a>
